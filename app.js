04e7048548e26a53396a053923487b55cc14d703
syke2good <akashbhandari930@gmail.com>
2022-02-20 14:14:23 -0800

    added a loginpage

--- app.js
+++ app.js
@@ -82,3 +82,13 @@ scrollLinks.forEach((link) => {
   });
 });
 // calculate heights
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
